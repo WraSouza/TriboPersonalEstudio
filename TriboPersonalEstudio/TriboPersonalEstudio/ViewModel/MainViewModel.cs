be24078f63e473406c503c7a5105a9ca22698cdb
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2022-01-28 17:16:52 -0300

    Adicionar arquivos de projeto.

---
+++ TriboPersonalEstudio/TriboPersonalEstudio/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TriboPersonalEstudio.ViewModel
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        public Command OpenLogin { get; set; }
+
+        public MainViewModel()
+        {
+            OpenLogin = new Command( () => OpenLoginView());
+        }
+
+        private void OpenLoginView()
+        {
+            Application.Current.MainPage = new View.LoginView();
+        }
+    }
+}
